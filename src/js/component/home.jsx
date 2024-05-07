f704515810ce10b13d99f3555311a6119cee209a
mariasr79 <147938132+mariasr79@users.noreply.github.com>
2024-05-07 16:08:22 +0000

    change

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,15 +9,13 @@ const Home = () => {
 	const [task, setTask] = useState("");
 	const [tasks, setTasks] = useState([]);
 	const [user, setUser] = useState();
-    
 
 
 
 	const createUser = async () => {
 		await fetch("https://playground.4geeks.com/todo/users/mariasr-79",
-			{method: "POST"}
+			{ method: "POST" }
 		).then(resp => {
-			
 			if (resp.ok) {
 				alert("se ha creado el usuario correctamente")
 				getUser();
@@ -29,8 +27,7 @@ const Home = () => {
 		await fetch("https://playground.4geeks.com/todo/users/mariasr-79").then(resp => {
 			console.log(resp)
 			if (!resp.ok) {
-				console.log("entramos")
-				return createUser();
+				createUser();
 			}
 			return resp.json()
 		}).then(user => setUser(user))
@@ -38,14 +35,14 @@ const Home = () => {
 
 	useEffect(() => {
 		getUser();
-		//createUser();
 	}, [])
 
 	const createTask = async (task) => {
-		await fetch("https://playground.4geeks.com/todo//users/mariasr-79", {
+		await fetch("https://playground.4geeks.com/todo/todos/mariasr-79", {
 			method: "POST",
-			header: {
-				contenType: "application/json"
+			headers: {
+				"content-type": "application/json"
+
 			},
 			body: JSON.stringify({
 				"label": task,
@@ -75,21 +72,21 @@ const Home = () => {
 
 	const deleteTask = async (task) => {
 		const id = task.id;
-		await fetch("https://playground.4geeks.com/todo//todos/${id}", {
+		await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
 			method: "DELETE"
 		}).then(resp => {
 			if (resp.ok) {
-				const userTaks = user.todos.filter(item.id !== task.id)
+				const userTaks = user.todos.filter(item => item.id !== task.id)
 				const newUser = {
 					...user,
 					todos: [...userTaks]
-				}
+				};
+				setUser(newUser)
 			}
 		})
 
 	}
 
-	console.log(user);
 
 
 
@@ -101,27 +98,30 @@ const Home = () => {
 				width: "200px",
 				position: "absolute",
 				top: "30px",
-				right: "30px"}} />
-		
-				<div className="todolist">
+				right: "30px"
+			}} />
+
+			<div className="todolist">
 				<input placeholder="add task"
 					onChange={(Event) => setTask(Event.target.value)}
 					onKeyDown={(Event) => Event.key === "Enter" && validateTask(task)}
 					type="text"
 					value={task} />
-				{user && user.todos.map((item) => <li key={item.id}>
+
+					{user && user.todos.map((item) =>
+					 <li key={item.id}>
 					{item.label}
-					<span onClick={() => deleteTask(item)}> <i class="fa-solid fa-x"></i></span>
+					<span onClick={() => deleteTask(item)}> <i className="fas fa-trash-alt"></i>
+					</span>
 				</li>)}
+
 			</div><p className="text-center">
 				{user && user.todos.length ?
 					<span>Tienes {user && user.todos.length} tareas pendientes</span> : <span> No hay tareas pendientes</span>}
 			</p>
-			<button onClick={() => deleteUser()}>
-				Delete User
-			</button>
-			</div>
-			</>
+			
+		</div>
+		</>
 	);
 }
 
